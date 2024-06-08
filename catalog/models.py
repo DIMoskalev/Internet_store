bd5b12771544f5f12691e59f92a8ee662c76c5ed
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-08 14:16:24 +0300

    create empty models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
+
+
+class Category(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
