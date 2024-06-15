c086898fdd60193b6313fd8b5621e8e4dca953df
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-15 16:45:51 +0300

    delete useless urlpattern

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,6 @@ from catalog.views import home, contacts, product_list, product_detail
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('', product_list, name='products_list'),
     path('products/<int:pk>', product_detail, name='product_detail'),
