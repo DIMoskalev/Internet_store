aad60c5fdc81295833d418f9a6307b18531f1f90
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-15 16:20:07 +0300

    add product_detail and product_list controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -12,3 +14,15 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'Вы получили сообщение от {name} (Номер телефона:{phone}): {message}')
     return render(request, 'contacts.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
