dd3338c25c260924c7b8f7a5fe1c7bb15889480c
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-20 23:27:29 +0300

    change urls bu CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_list, product_detail
+from catalog.views import ContactView, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('contacts/', contacts, name='contacts'),
-    path('', product_list, name='products_list'),
-    path('products/<int:pk>', product_detail, name='product_detail'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
