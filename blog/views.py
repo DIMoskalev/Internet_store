6bb1ffe5632fcb0c25343aebd730c3ec3a9ede29
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-21 01:01:38 +0300

    create classes for blog by CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from blog.models import BlogArticle
+
+
+class BlogArticleCreateView(CreateView):
+    model = BlogArticle
+    fields = ('title', 'content', 'preview', )
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogArticleListView(ListView):
+    model = BlogArticle
+
+
+class BlogArticleDetailView(DetailView):
+    model = BlogArticle
+
+
+class BlogArticleUpdateView(UpdateView):
+    model = BlogArticle
+    fields = ('title', 'content', 'preview', )
+
+
+class BlogArticleDeleteView(DeleteView):
+    model = BlogArticle
+    success_url = reverse_lazy('blog:blog_list')
