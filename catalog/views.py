b9f9a1de54674354858b2b9ddd55c5ab75e5aac6
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-06-20 23:27:52 +0300

    change FBV to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,28 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+# from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'home.html')
+class ProductListView(ListView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Вы получили сообщение от {name} (Номер телефона:{phone}): {message}')
-    return render(request, 'contacts.html')
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'products_list.html', context)
-
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_detail.html', context)
+class ContactView(TemplateView):
+    template_name = "catalog/contacts.html"
+    # model = Contact
+    # fields = ('name', 'phone', 'message',)
+    # success_url = reverse_lazy('catalog:contacts')
