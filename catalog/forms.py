8381eb909ebba94578b11dbbfd92a7e25b3ef0f7
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-04 22:53:23 +0300

    fix bug with checkbox

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,9 +8,7 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            if isinstance(field, BooleanField):
-                field.widget.attrs['class'] = 'form-check-input'
-            else:
+            if not isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-control'
 
 
