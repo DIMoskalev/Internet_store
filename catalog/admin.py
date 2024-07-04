4e905cc39b9b1e52f8e004aee272809994e5cf20
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-04 22:09:28 +0300

    add VersionAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
 
 
 @admin.register(Product)
@@ -13,3 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'version_number', 'name', 'is_active')
+    list_filter = ('product', 'is_active')
+    search_fields = ('product', 'name')
