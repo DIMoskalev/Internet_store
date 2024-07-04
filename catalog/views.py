816ec3ce6c98733ce4d97e940ba14cb123e9275b
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-04 20:06:24 +0300

    Implemented CRUD model for model Product

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-# from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from catalog.forms import ProductForm
 
 from catalog.models import Product
 
@@ -7,9 +8,38 @@ from catalog.models import Product
 class ProductListView(ListView):
     model = Product
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = 'Каталог'
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактирование товара'
+        return context
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ContactView(TemplateView):
