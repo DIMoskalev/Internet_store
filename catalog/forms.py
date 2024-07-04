819b84de3b9cc7739d7ba40a2f76b26107b6cc31
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-04 22:10:14 +0300

    create VersionForm and StyleFormMixin and add StyleFormMixin to all forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms import BooleanField
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
@@ -25,3 +36,9 @@ class ProductForm(forms.ModelForm):
             if banned_word in cleaned_data.lower():
                 raise forms.ValidationError(f'Нельзя использовать слово "{banned_word}" для описания товара')
         return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
